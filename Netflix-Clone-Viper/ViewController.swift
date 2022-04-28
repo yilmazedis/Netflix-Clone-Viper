aca42c6f8b2a9d819eec869f3eb472cb97d68deb
Yilmaz Edis <ylmazedis@gmail.com>
2022-04-28 16:11:43 +0300

    Initial Commit

---
+++ Netflix-Clone-Viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone-Viper
+//
+//  Created by yilmaz on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
